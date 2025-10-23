a00b98bdc4dc62cf3dc235d920c3eed8945af608
FidelMH <moussahaziri.fidel@gmail.com>
2025-10-23 11:15:09 +0400

    feat: CRUD pour matière

--- php-crud/model/matieres.php
+++ php-crud/model/matieres.php
@@ -1,5 +1,29 @@
 <?php
 
-namespace Models
+namespace Models;
+
+use Config\Database;
+
+class Matiere{
+    // private $id_matieres;
+    // private $nom_matieres;
+    // private $description_matiere;
+    private $conn;
+
+    public function __construct(){
+        $database = new Database();
+        $this->conn = $database->connect();
+    }
+
+    public function read(){
+
+        $sql = "SELECT * FROM matieres";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+    }
+
+
+}
 
 ?>
