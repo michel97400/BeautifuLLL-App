0b9588ad876d441094f73a048ad0f981256c7127
michel payet <michel.payet974@live.fr>
2025-10-24 08:44:52 +0400

    ajout model llama

--- php-crud/controllers/SessionConversationController.php
+++ php-crud/controllers/SessionConversationController.php
@@ -1,32 +1,34 @@
 <?php
 
+
 namespace Controllers;
 
+require_once __DIR__ . '/../model/SessionConversation.php';
 use Models\session_conversation;
 
 class SessionConversationController
 {
     public function createSessionConversation($duree_session, $date_heure_fin, $id_agents, $id_etudiant)
     {
-        $SessionConversation = new SessionConversation();
+        $SessionConversation = new session_conversation();
         return $SessionConversation->create($duree_session, $date_heure_fin, $id_agents, $id_etudiant);
     }
 
     public function getSessionConversation()
     {
-        $SessionConversation = new SessionConversation();
+        $SessionConversation = new session_conversation();
         return $SessionConversation->read();
     }
 
     public function getSingleSessionConversation($id_session)
     {
-        $SessionConversation = new SessionConversation();
+        $SessionConversation = new session_conversation();
         return $SessionConversation->readSingle($id_session);
     }
 
     public function updateSessionConversation($id_session, $date_heure_debut, $duree_session, $date_heure_fin, $id_agents, $id_etudiant)
     {
-        $SessionConversation = new SessionConversation();
+        $SessionConversation = new session_conversation();
         return $SessionConversation->update($id_session, 
             $date_heure_debut, 
             $duree_session, 
@@ -37,7 +39,7 @@ class SessionConversationController
 
     public function deleteSessionConversation($id_Session)
     {
-        $SessionConversation = new SessionConversation();
+        $SessionConversation = new session_conversation();
         return $SessionConversation->delete($id_Session);
     }
 }
