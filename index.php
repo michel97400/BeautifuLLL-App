1b9a4f69bb21319140dd934686f13d9140f39dfb
FidelMH <moussahaziri.fidel@gmail.com>
2025-10-26 18:27:30 +0400

    styled the static  ai assistant chat page,

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $isAdmin = $user && isset($user['role']) && $user['role'] === 'Administrateur';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beautiful AI</title> <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_upward_alt" />
 </head>
 <body>
     <main>
